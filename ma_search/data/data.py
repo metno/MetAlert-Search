1d9b0ff62d105b31bd314bf53a6a750ea12a2d95
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-06-25 16:58:57 +0200

    Make data class a placeholder so it can be implemented after Shape is done

--- ma_search/data/data.py
+++ ma_search/data/data.py
@@ -18,79 +18,10 @@ limitations under the License.
 """
 
 import logging
-import ma_search
-
-from datetime import datetime
-
-from ma_search.db import SQLiteDB
 
 logger = logging.getLogger(__name__)
 
-MAP_DICT = {
-    "label": (str, True),
-    "source": (str, True),
-    "administrativeName": (str, False),
-    "administrativeID": (str, False),
-    "validFrom": (datetime, False),
-    "validTo": (datetime, False),
-    "polygon": ((dict, list), False),
-    "coordinateSystem": (str, True),
-}
-
-class Data():
-
-    def __init__(self):
-
-        self.conf = ma_search.CONFIG
-
-        self._db = None
-        if self.conf.dbProvider == "sqlite":
-            self._db = SQLiteDB()
-
-        return
-
-    ##
-    #  Methods
-    ##
-
-    def addMapEntry(self, data):
-        """Add a new map entry to the database.
-        Must be implemented in subclass.
-
-        Parameters
-        ----------
-        data : dict
-            A dictionary of the data to be added to the database.
-        """
-        raise NotImplementedError
-
-    def updateMapEntry(self, uuid, data):
-        """Update a map entry in the database.
-        Must be implemented in subclass.
-
-        Parameters
-        ----------
-        data : dict
-            A dictionary of the data to be added to the database.
-        uuid : str
-            The UUID of the entry to be updated
-        """
-        raise NotImplementedError
-
-    ##
-    #  Internal Functions
-    ##
-
-    def _validateMapDict(self, data):
-        """
-        """
-        valid = True
-        for key, req in MAP_DICT.items():
-            valid &= key in data
-            value = data[key]
-            if not isinstance(value, req[0]):
-                valid &= req[1] and value is None
-
-        return valid
+class Data(): # pragma: No cover
+    pass
 
 # END Class Data
