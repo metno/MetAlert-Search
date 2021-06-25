2d9a3de00ea4ed709325f87d5022d4ecb6ff6c8d
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-06-25 15:11:16 +0200

    Add a file for common functions, and a test for it

---
+++ ma_search/common.py
@@ -0,0 +1,51 @@
+"""
+MetAlert Search : Common Functions
+==================================
+
+Copyright 2021 MET Norway
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+def safeMakeDir(path):
+    """Create a folder and return if successful.
+
+    Parameters
+    ----------
+    path : str
+        The path to the folder to be created/checked.
+
+    Returns
+    -------
+    bool
+        True if successful, False otherwise.
+    """
+    if not isinstance(path, str):
+        return False
+
+    if os.path.isdir(path):
+        return True
+
+    try:
+        os.mkdir(path)
+    except Exception as e:
+        logger.error("Could not create: %s" % str(path))
+        logger.error(str(e))
+        return False
+
+    return True
---
+++ tests/test_core/test_common.py
@@ -0,0 +1,51 @@
+"""
+MetAlert Search : Common Functions Tests
+========================================
+
+Copyright 2021 MET Norway
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import pytest
+
+from tools import writeFile
+
+from ma_search.common import safeMakeDir
+
+@pytest.mark.core
+def testCoreCommon_SafeMakeDir(tmpDir, caplog):
+    """Test the safeMakeDir function.
+    """
+    newDir = os.path.join(tmpDir, "safemakedir")
+    newFile = os.path.join(tmpDir, "safemakedir", "file.txt")
+    # Wrong type
+    assert safeMakeDir(None) is False
+
+    # Success
+    assert safeMakeDir(newDir) is True
+    assert os.path.isdir(newDir)
+
+    # Try again, should result in success
+    assert safeMakeDir(newDir) is True
+    assert os.path.isdir(newDir)
+
+    # Make a dir where a file exists
+    writeFile(newFile, "stuff")
+    assert os.path.isfile(newFile)
+    caplog.clear()
+    assert safeMakeDir(newFile) is False
+    assert "Could not create: %s" % newFile in caplog.text
+
+# END Test testCoreCommon_SafeMakeDir
