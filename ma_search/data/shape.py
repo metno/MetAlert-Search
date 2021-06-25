72cc7a891fa5efaf4028231d4c8c5fe0bcd786e5
Fabio Zeiser <fabio.zeiser@met.no>
2021-06-25 14:19:49 +0200

    started on Shape class

--- ma_search/data/shape.py
+++ ma_search/data/shape.py
@@ -17,37 +17,59 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import uuid
+import json
 import logging
+from pathlib import Path
+from uuid import UUID, uuid4
 
 import ma_search
+from shapely.geometry import MultiPolygon, Polygon, mapping, shape
 
 logger = logging.getLogger(__name__)
 
-class Shape(): # pragma: no cover
-
+class Shape():
     def __init__(self, uuid):
+        """Keeps track, can lookup and manipulate (Multi)Polygons via their uuid
 
+        Args:
+            uuid (str): Unique identifyer UUID4
+        """
         self.conf = ma_search.CONFIG
         self._uuid = uuid
+        if not self._validateUuid():
+            logger.error("UUID %s is not valid", self._uuid)
+            return
 
-        # ToDo:
-        # - Validate UUID
-        # - Check that file exists
-
-        return
+        self._path = (Path(self.conf.dataPath) / self._uuid).with_suffix(".geojson")
+        if not self._path.exists():
+            logger.error("UUID file %s does not exist", self._path)
 
     @classmethod
     def fromGeoJSON(cls, data):
-        """
+        """Returns Shape instance from a GeoJson input file
+
+        Parameters
+        ----------
+        data : dict or Path or str
+            Input data, either a single GeoJson feature, or only the geometry
+            section of a single feature. If str or Path: Path to the file
+            containing this dict.
+
         Returns
         -------
-        :obj:`Shape` :
-            A new instance of Polygon()
+        :obj:`Shape` or None:
+            A new instance of Shape(). Returns None for invalid input, e.g. not
+            a Polygon.
         """
-        newUUID = uuid.uuid4()
-        # save geojson data to file named newUUID.geojson
-        poly = cls(newUUID)
+        uuid = uuid4()
+        if cls.polygonFromGeoJson(data) is None:
+            return None # tests that it
+
+        path = (ma_search.CONFIG.dataPath / str(uuid)).with_suffix(".geojson")
+        with path.open('w') as f:
+            json.dump(data, f)
+
+        poly = cls(str(uuid))
         return poly
 
     ##
@@ -60,8 +82,8 @@ class Shape(): # pragma: no cover
         Parameters
         ----------
         tolerance : float
-            The tolerance level to return. If 0, retunrs the full
-            polygon.
+            The tolerance level to return. If 0, rereturns the "full"
+            polygon. Assumes that the minimum tolerance is 1e-6.
         cachedOnly : bool
             If False, will generate the simplified polygon if tolerance
             is larger than zero. If True, will return None if the
@@ -70,23 +92,124 @@ class Shape(): # pragma: no cover
         Returns
         -------
         :obj:`shapely.Polygon`, :obj:`shapely.MultiPolygon` or None
+            Returns shape if it can be found, otherwise None.
         """
-        return
+        if tolerance == 0.0:
+            return self.polygonFromGeoJson(self._path)
+        else:
+            suffix = f".{round(tolerance * 1e6)}.geojson"
+            path = (self._path.parent / self._path.stem).with_suffix(suffix)
+            exists = path.exists()
+
+            if exists:
+                return self.polygonFromGeoJson(path)
+            elif cachedOnly:
+                logger.error("Polygon does not exist for tolerance %s", tolerance)
+                return
+            elif not exists:
+                logger.info("Creating polygon %s with tolerance %s", self._uuid, tolerance)
+                full = self.polygonFromGeoJson(self._path)
+                simple = full.simplify(tolerance)
+                with path.open('w') as f:
+                    json.dump(mapping(simple), f)
+                return simple
+
+    def toGeoJson(self, **kwargs):
+        """Return geometry as as GeoJson
+        Parameters
+        ----------
+        kwargs : dict
+            kwargs to pass to Shape.polygon()
+        """
+        geom = mapping(self.polygon(**kwargs))
+        geoJson = {"type": "Feature",
+                   "geometry": geom}
+        return geoJson
 
     ##
     #  Static Methods
     ##
 
     @staticmethod
-    def polygonFromGeoJson(geoJson):
+    def polygonFromGeoJson(data):
+        """Returns the polygon extracted from the geojson file.
+
+        Parameters
+        ----------
+        data : dict or Path or string
+
+        Returns
+        -------
+        :obj:`shapely.Polygon`, :obj:`shapely.MultiPolygon` or None
+            Returns shape if it can be found, otherwise None.
         """
-        """
-        return
+        if isinstance(data, dict):
+            pass
+        elif isinstance(data, (Path, str)):
+            path = Path(data)
+            if not path.exists():
+                logger.error("%s does not exist", path)
+                return None
+            with path.open() as f:
+                data = json.load(f)
+        else:
+            logger.error("Input has to be dict, Path or string but is of type %s", type(data))
+            return None
+
+        if "geometry" in data:
+            data = data["geometry"]
+        try:
+            geom = shape(data)
+            if not isinstance(geom, (Polygon, MultiPolygon)):
+                raise TypeError("data is not a polygon but %s", type(geom))
+        except Exception as e:
+            logger.error("Not a valid GeoJson(-like) dict. %s: %s", type(e).__name__, e)
+            return None
+        return geom
 
     @staticmethod
-    def geoJsonFromPolygon(polygon):
-        """
-        """
-        return
+    def geoJsonFromPolygon(polygon, extra={}):
+        """Returns geoJson from a shapely object
 
+        Parameters
+        ----------
+        polygon : :obj:`shapely.Polygon`, :obj:`shapely.MultiPolygon`
+            Shapely object (Polygon).
+        extra : dict
+            Additional information to add to the geoJson file by updating the
+            dictionary.
+
+        Returns
+        -------
+        dict
+        """
+        geoJson = {"type": "Feature",
+                   "geometry": mapping(polygon)}
+        if "type" in extra or "geometry" in extra:
+            logging.warning("Cannot append extra arguments due to overlap in keys.")
+            return geoJson
+        else:
+            geoJson.update(extra)
+            return geoJson
+
+    ##
+    #  Internal Methods
+    ##
+
+    def _validateUuid(self):
+        """Validates that a string is a valid uuid4."""
+        if not isinstance(self._uuid, str):
+            logger.error("UUID %s is not a string", self._uuid)
+            return False
+
+        try:
+            val = UUID(self._uuid)
+            if str(val) == self._uuid:
+                return True
+            else:
+                logger.error("UUID %s is not valid", self._uuid)
+                return False
+        except Exception as e:
+            logger.error("%s: %s", type(e).__name__, e)
+            return False
 # END Class Shape
--- tests/test_data/test_shape.py
+++ tests/test_data/test_shape.py
@@ -17,13 +17,239 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import pytest
+import json
+from pathlib import Path
 
+import pytest
+import shapely.geometry as geometry
 from ma_search.data.shape import Shape
+from shapely.geometry import MultiPolygon, Polygon
+
 
 @pytest.mark.data
-def testDataShape_Init():
+def testDataShape_Init(tmpConf, tmpDir, caplog):
+
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    # initialization runs
     shape = Shape("")
+    shape._validateUuid4 = lambda: None
     assert isinstance(shape, Shape)
 
+    # check error if file does not exist
+    caplog.clear()
+    uuid = '33d0c48f-b58c-4b1a-b224-e93b03393cb3'
+    shape = Shape(uuid)
+    assert "does not exist" in caplog.text
+
+    # but runs if it exists
+    Path(dataPath/uuid).with_suffix(".geojson").touch()
+    caplog.clear()
+    shape = Shape(uuid)
+    assert caplog.text == ""
+
+    # shape._validateUuid4 = lambda: None
+
+@pytest.mark.parametrize("fn",
+    ["simple_Fylker_0.json", "simple_Kommuner_0.json", "simple_Kommuner_291.json"]) # noqa
+@pytest.mark.data
+def testDataShape_FromGeoJson(fn, tmpConf, tmpDir, caplog, monkeypatch):
+    """ Checks that geojson file is stored and Shape instance is created"""
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    fn = Path("tests/test_data/data") / fn
+    with fn.open() as f:
+        data = json.load(f)
+        data = data["polygon"]
+
+    # correct instance created
+    shape = Shape.fromGeoJSON(data)
+    assert isinstance(shape, Shape)
+    assert shape._path.exists()
+
+    # returns None if not valid input
+    monkeypatch.setattr(Shape, "polygonFromGeoJson", lambda x: None)
+    shape = Shape.fromGeoJSON("")
+    assert shape is None
+
+
+@pytest.mark.data
+def testDataShape_Polygon(tmpConf, tmpDir, caplog):
+    """ Checks that geojson file is stored and Shape instance is created"""
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    fn = "simple_Fylker_0.json"
+    fn = Path("tests/test_data/data") / fn
+    with fn.open() as f:
+        data = json.load(f)
+        data = data["polygon"]
+
+    uuid = '33d0c48f-b58c-4b1a-b224-e93b03393cb3'
+    shape = Shape(uuid)
+
+    # create file ans see if it is returned:
+    path = dataPath / f'{uuid}.geojson'
+    with path.open('w') as f:
+        json.dump(data, f)
+    result = shape.polygon()
+    assert isinstance(result, (Polygon, MultiPolygon))
+
+    # error if it does exist (default)
+    caplog.clear()
+    tolerance = 50.5
+    tolerance_str = str(50_500_000)
+    fnjson = (dataPath / f'{uuid}.{tolerance_str}.geojson')
+    (dataPath / f'{uuid}.50.5.geojson').unlink(missing_ok=True)
+    result = shape.polygon(tolerance=tolerance)
+    assert "Polygon does not exist for tolerance" in caplog.text
+
+    # but create it, if cachedOnly=False
+    caplog.clear()
+    (dataPath / f'{uuid}.50.5.geojson').unlink(missing_ok=True)
+    result = shape.polygon(tolerance=50.5, cachedOnly=False)
+    assert "Creating polygon" in caplog.text
+    assert fnjson.exists()
+
+    # now it exists -- check that it is loaded
+    shape.polygonFromGeoJson = lambda path: path
+    result = shape.polygon(tolerance=50.5, cachedOnly=False)
+    assert result == fnjson
+
+
+@pytest.mark.parametrize("fn",
+    ["simple_Fylker_0.json", "simple_Kommuner_0.json", "simple_Kommuner_291.json"]) # noqa
+@pytest.mark.data
+def testDataShape_ToGeoJson(fn, tmpConf, tmpDir):
+    """ Checks that geojson file is stored and Shape instance is created"""
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    fn = Path("tests/test_data/data") / fn
+    with fn.open() as f:
+        data = json.load(f)
+        data = data["polygon"]
+
+    shape = Shape.fromGeoJSON(data)
+    result = shape.toGeoJson()
+    expected = {"type": "Feature", "geometry": data}
+    # TODO:
+    # hacky comparison, but convert tuples to lists
+    result = eval(result.__repr__().replace("(", "[").replace(")", "]"))
+    assert result == expected
+
+
+@pytest.mark.parametrize("file, typ", [("simple_Fylker_0.json", Polygon),
+                                       ("simple_Kommuner_0.json", Polygon),
+                                       ("simple_Kommuner_291.json", MultiPolygon)])
+@pytest.mark.data
+def testDataShape_PolygonFromGeoJson(file, typ, tmpConf, tmpDir, caplog):
+    """ Checks that geojson file is stored and Shape instance is created"""
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    fn = Path("tests/test_data/data") / file
+    with fn.open() as f:
+        data = json.load(f)
+        data = data["polygon"]
+        fn_geodata = dataPath / file
+        with fn_geodata.open('w') as f:
+            json.dump(data, f)
+
+    # correct instance created
+    shape = Shape.polygonFromGeoJson(data)
+    assert isinstance(shape, typ)
+    assert Shape.polygonFromGeoJson(data) == Shape.polygonFromGeoJson(fn_geodata)
+
+    # works also on True geoJson
+    data_true_geoJson = {"type": "Feature",
+                         "geometry": data,
+                         "properties": {"name": "Dinagat Islands"}}
+    shape = Shape.polygonFromGeoJson(data_true_geoJson)
+    assert isinstance(shape, typ)
+
+    # input typechecks
+    data_true_geoJson = {"type": "Feature",
+                         "geometry": data,
+                         "properties": {"name": "Dinagat Islands"}}
+    caplog.clear()
+    shape = Shape.polygonFromGeoJson([(0, 1), (1, 0), (0, 1)])
+    assert shape is None
+    assert "Input has to be dict" in caplog.text
+
+    # error if not (Multipolygon)
+    caplog.clear()
+    shape = Shape.polygonFromGeoJson({"type": "Point", "coordinates": [125.6, 10.1]})
+    assert shape is None
+    assert "Not a valid GeoJson" in caplog.text
+
+    # Err if path does not exist
+    caplog.clear()
+    shape = Shape.polygonFromGeoJson("nopath/to/file")
+    assert "does not exist" in caplog.text
+
+
+@pytest.mark.parametrize("fn",
+    ["simple_Fylker_0.json", "simple_Kommuner_0.json", "simple_Kommuner_291.json"]) # noqa
+@pytest.mark.data
+def testDataShape_GeoJsonFromPolygon(fn, tmpConf, tmpDir, caplog):
+    """ Checks that geojson file is stored and Shape instance is created"""
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    fn = Path("tests/test_data/data") / fn
+    with fn.open() as f:
+        data = json.load(f)
+        data = data["polygon"]
+
+    # plain comparison
+    data = geometry.shape(data)
+    expected = {"type": "Feature", "geometry": geometry.mapping(data)}
+    result = Shape.geoJsonFromPolygon(data)
+    assert result == expected
+
+    # added arguments
+    pars = {"parameters": {"name": "foo"}, "crs" : 123}
+    expected = {"type": "Feature", "geometry": geometry.mapping(data),
+                "parameters": {"name": "foo"}, "crs" : 123}
+    result = Shape.geoJsonFromPolygon(data, pars)
+    assert result == expected
+
+    # warn if special key
+    pars = {"geometry": 123, "crs" : 123}
+    expected = {"type": "Feature", "geometry": geometry.mapping(data)}
+    caplog.clear()
+    result = Shape.geoJsonFromPolygon(data, pars)
+    assert result == expected
+    assert "Cannot append" in caplog.text
+
+
+@pytest.mark.data
+@pytest.mark.parametrize("uuid,expected,logtext",
+    [(None, False, "is not a string"), # noqa # not a string
+     ('33d0c48f-b58c-4b1a-b224-e93b03393cb3', True, ""), # gen by uuid4
+     ('33d0c48fb58c4b1ab224e93b03393cb3', False, ""), # missing '-' character
+     ('0', False, "ValueError")]) # check catching error
+def testDataShape_ValidateUudi4(uuid, expected, logtext, caplog, tmpConf, tmpDir):
+    """Test that the UUIDs are validated or errors are logged.
+    """
+    dataPath = Path(tmpDir) / "data"
+    dataPath.mkdir(exist_ok=True)
+    tmpConf.dataPath = dataPath
+
+    shape = Shape(uuid)
+    caplog.clear()
+
+    result = shape._validateUuid()
+    assert result == expected
+    if not result:
+        assert logtext in caplog.text
 # END Test testDataShape_Init
