e644e6ecedbef3bf2a925ea69ef629595e3c8641
Johannes Tobiassen Langvatn <21368486+johtoblan@users.noreply.github.com>
2021-09-27 15:24:17 +0200

    Fix failing tests

--- tests/test_utils/test_ingest_cap.py
+++ tests/test_utils/test_ingest_cap.py
@@ -18,30 +18,78 @@ limitations under the License.
 """
 
 import os
+import json
 import pytest
+
+from tools import writeFile
 
 from ma_search.utils import ingestCap
 
 
 @pytest.mark.utils
-def testUtil_IngestCap(filesDir):
+def testUtil_IngestCap(filesDir, fncDir, tmpConf, caplog):
     """Tests ingestCap function in ma_search.utils, mainly covering
     lines, since ingestCap should be non-failing
     """
+    tmpConf.dataPath = fncDir
+
+    # Make a valid file
+    testCap = os.path.join(fncDir, "good.cap.xml")
+    writeFile(testCap, (
+        "<alert>"
+        "<identifier>mockAlert</identifier>"
+        "<info>"
+        "<area>"
+        "<polygon>1,1 1,2 2,2 2,1 1,1</polygon>"
+        "<altitude>0</altitude>"
+        "<ceiling>1</ceiling>"
+        "</area>"
+        "</info>"
+        "</alert>"
+    ))
+
     with pytest.raises(SystemExit):
         ingestCap(["--notACommand"])
     with pytest.raises(SystemExit):
         ingestCap(["-h"])
 
-    # Folder input
-    ingestCap([filesDir])
-    ingestCap(["--overwrite", filesDir])
-    ingestCap(["--overwrite", "--recursive", filesDir])
+    ingestCap([testCap])
 
-    # Single file input
-    filePath = os.path.join(filesDir, "mock.cap.xml")
-    ingestCap([filePath])
-    ingestCap(["--overwrite", filePath])
+    # Check the JSON file
+    jsonFile = os.path.join(
+        fncDir, "alert_4", "alert_f", "a35e85f4-b0d1-5b1f-9db0-79007f49be07.json"
+    )
+    assert os.path.isfile(jsonFile)
+    with open(jsonFile, mode="r") as inFile:
+        jsonData = json.load(inFile)
+
+    assert jsonData["identifier"] == "mockAlert"
+    assert jsonData["source"] == testCap
+    assert jsonData["polygon"]["type"] == "Polygon"
+    assert jsonData["polygon"]["coordinates"] == [[
+        [1.0, 1.0], [2.0, 1.0], [2.0, 2.0], [1.0, 2.0], [1.0, 1.0]
+    ]]
+    assert jsonData["altitude"] == 0.0
+    assert jsonData["ceiling"] == 1.0
+    assert jsonData["area"] == 1.0
+    assert jsonData["bounds"]["east"] == 2.0
+    assert jsonData["bounds"]["west"] == 1.0
+    assert jsonData["bounds"]["north"] == 2.0
+    assert jsonData["bounds"]["south"] == 1.0
+
+    # Ingest again
+    caplog.clear()
+    ingestCap([testCap])
+    assert (
+        "CAP file with identifier 'mockAlert' already exists and is not being overwritten"
+    ) in caplog.text
+
+    # Ingest overwrite
+    caplog.clear()
+    ingestCap(["--overwrite", testCap])
+    assert (
+        "CAP file with identifier 'mockAlert' already exists and is not being overwritten"
+    ) not in caplog.text
 
     # Path to file that does not exist
     ingestCap([os.path.join(filesDir, "nonExistentFile.xml")])
